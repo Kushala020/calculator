2fe11bff992cd3e7d1aafc6212e1bff2ac8c3435
Kushala020 <kushalar020@gmail.com>
2026-09-11 10:11:51 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,13 @@
+#Task 1 : basic mathematical operations
+num1=float(input("enter first number :",))
+num2=float(input("enter second number :",))
+print("\n addition:", num1+num2)
+print("subtratction:", num1-num2)
+print("multiplication:", num1*num2)
+print("division:", num1/num2)
+
+#task2 : creating personalised greeting
+first_name=(input("enter your first name:"))
+last_name=(input("enter your last name:"))
+full_name=first_name+" "+last_name
+print("hello", full_name ,"welcome to the python program.")

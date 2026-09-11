26f1d396215862cfecffbb7f50b086f63f1cb637
Kushala020 <kushalar020@gmail.com>
2026-09-11 12:26:15 +0530

    Delete arithematic operators.py

--- arithematic operators.py
+++
@@ -1,7 +0,0 @@
-# arithematic operators
-num1=float(input("enter first number :",))
-num2=float(input("enter second number :",))
-print("\n addition:", num1+num2)
-print("subtratction:", num1-num2)
-print("multiplication:", num1*num2)
-print("division:", num1/num2)

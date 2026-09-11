68a2d1497d30be0b41d42fcf041ac5a4d051b248
Kushala020 <kushalar020@gmail.com>
2026-09-11 12:01:56 +0530

    Add files via upload

---
+++ # arithematic operators.py
@@ -0,0 +1,7 @@
+# arithematic operators
+num1=float(input("enter first number :",))
+num2=float(input("enter second number :",))
+print("\n addition:", num1+num2)
+print("subtratction:", num1-num2)
+print("multiplication:", num1*num2)
+print("division:", num1/num2)
